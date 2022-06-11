14b7812b1afdc56c98df92e3830e5f91bb9e02c0
sayitsamikoca <sayitsamikoca0@gmail.com>
2022-06-11 14:21:00 +0300

    Login Teması Projeye Entegre Edildi.

---
+++ ProjeCore/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjeCore.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GirisYap()
+        {
+            return View();
+        }
+    }
+}
