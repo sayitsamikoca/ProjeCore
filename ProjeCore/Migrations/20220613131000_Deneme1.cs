655a88e9eb010824937578d3cb8357c69eb110e4
sayitsamikoca <sayitsamikoca0@gmail.com>
2022-06-13 16:13:32 +0300

    Admin tablosu db'ye eklendi

---
+++ ProjeCore/Migrations/20220613131000_Deneme1.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProjeCore.Migrations
+{
+    public partial class Deneme1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Admins",
+                columns: table => new
+                {
+                    AdminID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Kullanici = table.Column<string>(type: "Varchar(20)", nullable: true),
+                    Sifre = table.Column<string>(type: "Varchar(10)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Admins", x => x.AdminID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Admins");
+        }
+    }
+}
