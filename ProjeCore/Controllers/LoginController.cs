226787935479386c4bb27bb7b5b8aa049fd35cff
sayitsamikoca <sayitsamikoca0@gmail.com>
2022-06-19 14:34:25 +0300

    Login İşlemleri Yazıldı. Proje Bitişi

--- ProjeCore/Controllers/LoginController.cs
+++ ProjeCore/Controllers/LoginController.cs
@@ -1,17 +1,55 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using ProjeCore.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
 
 namespace ProjeCore.Controllers
 {
     public class LoginController : Controller
     {
-        public IActionResult Index()
+        Context context = new Context();
+
+        [HttpGet]
+        public IActionResult GirisYap()
         {
             return View();
         }
 
-        public IActionResult GirisYap()
+        public async Task<IActionResult> GirisYap(Admin admin)
         {
+            // Identity kullanacağımız için devreye async ifadeler girer.
+
+            var bilgiler = context.Admins.FirstOrDefault(x => x.Kullanici == admin.Kullanici && x.Sifre == admin.Sifre);
+
+            if (bilgiler != null) {
+
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name , admin.Kullanici)
+                };
+
+                var userIdentity = new ClaimsIdentity(claims, "Login");
+                ClaimsPrincipal principal = new ClaimsPrincipal(userIdentity);
+
+                await HttpContext.SignInAsync(principal); 
+                return RedirectToAction("Index", "Personels");
+
+
+
+
+                /* Notes!
+                 * bilgilerden null gelmezse burası çalışır aksi durumda View'i return eder.
+                 * İnputlara girmiş olduğum kullanici degeriyle sifre değeri eşleşirse null dönmeyecek.
+                 * claims: talepler
+                 * ClaimsPricipal: Birden çok talep tabanlı kimliği destekleyen app.
+                 * async bir ifadeyi kullanabilmek için önüne 'await' koymak gerekli.
+                 */
+            }
             return View();
+
         }
     }
 }
--- ProjeCore/Startup.cs
+++ ProjeCore/Startup.cs
@@ -38,6 +38,8 @@ namespace ProjeCore
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseAuthentication();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
